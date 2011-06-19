d1f4125a09a85b55b38ae2b01191d5222a860357
Théo Reichel <theo@complex.ch>
2011-06-19 16:24:01 +0200

    wip

--- mode1.py
+++ mode1.py
@@ -58,8 +58,8 @@ class ScreenSaver(Widget):
     self.add_widget(self.img)
     
     # cartel
-    self.cartel = Label(text="PlaneWhite", pos=(600,650), font_size=40, color=(1,1,1,1))
-    self.touch_me = Label(text="try me if you dare!", pos=(575,600), font_size=20, color=(1,1,1,1))
+    self.cartel = Label(text="PlaneWhite", pos=(600,650), font_size=40, color=(1,1,1,1), halign="right")
+    self.touch_me = Label(text="try me if you dare!", pos=(600,600), font_size=20, color=(1,1,1,1), halign="right")
     self.add_widget(self.cartel)
     self.add_widget(self.touch_me)
     
