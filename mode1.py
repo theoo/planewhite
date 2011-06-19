2a566877d12b3b046fae1102e8fa7cb1bd116b02
Théo Reichel <theo@complex.ch>
2011-06-19 20:10:18 +0200

    changing delay

--- mode1.py
+++ mode1.py
@@ -20,7 +20,7 @@ import lib.config, lib.kwargs
 
 # Configuration
 SCAN_IMG_PATH = 'images/scan.png'
-SCAN_DURATION = 2.0
+SCAN_DURATION = 1.5
 NETWORK_DELAY = 0 # frames
 TRIGGER_POINTS_THRESHOLD = 300
 
