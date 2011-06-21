c9d83dbe07b4694b480e91faf890734b559c6068
Théo Reichel <theo@complex.ch>
2011-06-21 23:47:00 +0200

    wip

--- mode1.py
+++ mode1.py
@@ -125,6 +125,7 @@ class ScreenSaver(Widget):
     a1.bind(on_progress=self.syncServerCommunication)
     a1.bind(on_complete=self.remove_mask)
     a1.start(self.scanner)
+    print self.pos
     print self.children
     print self.children.count(self.scanner)
 
@@ -132,7 +133,7 @@ class ScreenSaver(Widget):
     self.add_widget(self.mask)
 
   def remove_mask(self, target, dt):
-    self.scanner.pos = (self.pos[0] - self.scanner.width * 2,0)
+    self.scanner.pos = (self.pos[0] - self.scanner.width,0)
     self.remove_widget(self.mask)
 
   def draw_ellipse(self):
