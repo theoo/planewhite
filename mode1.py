e7dd67288cad7c9fa9a79b014df70293b7030e00
Théo Reichel <theo@complex.ch>
2011-06-21 23:48:50 +0200

    wip

--- mode1.py
+++ mode1.py
@@ -120,6 +120,7 @@ class ScreenSaver(Widget):
 
 # Custom methods
   def scan(self, dt):
+    self.scanner.pos = (self.pos[0] - self.scanner.width,0)    
     a1 = Animation(pos=(self.pos[0] + self.width, 0), duration=self.scan_duration)
     a1.bind(on_start=self.add_mask)
     a1.bind(on_progress=self.syncServerCommunication)
@@ -133,7 +134,6 @@ class ScreenSaver(Widget):
     self.add_widget(self.mask)
 
   def remove_mask(self, target, dt):
-    #self.scanner.pos = (self.pos[0] - self.scanner.width,0)
     self.remove_widget(self.mask)
 
   def draw_ellipse(self):
