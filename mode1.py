f498fade2567f899d5728f6b8e1c0464ceb174f9
Théo Reichel <theo@complex.ch>
2011-06-21 21:03:51 +0200

    wip

--- mode1.py
+++ mode1.py
@@ -20,7 +20,7 @@ import lib.config, lib.kwargs
 
 # Configuration
 SCAN_IMG_PATH = 'images/scan.png'
-SCAN_DURATION = 5
+SCAN_DURATION = 5.0
 NETWORK_DELAY = 0 # frames
 TRIGGER_POINTS_THRESHOLD = 150
 
