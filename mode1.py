841a9989a4f523188f09cd71628974620c57c733
Théo Reichel <theo@complex.ch>
2011-06-21 23:54:46 +0200

    wip

--- mode1.py
+++ mode1.py
@@ -131,7 +131,7 @@ class ScreenSaver(Widget):
     self.add_widget(self.mask)
 
   def remove_mask(self, target, dt):
-    if clientID == 4: # quick and dirty hack
+    if clientId == 4: # quick and dirty hack
       self.scanner.pos = (self.pos[0] - self.scanner.width,0)
 
     self.remove_widget(self.mask)
