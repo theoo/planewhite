ec2ddff167ef964e23c3c8233c29b3197e04feb6
Théo Reichel <theo@complex.ch>
2011-06-21 22:47:14 +0200

    wip

--- mode1.py
+++ mode1.py
@@ -120,6 +120,7 @@ class ScreenSaver(Widget):
     a1.bind(on_complete=self.remove_scanner)
     a1.start(self.scanner)
     print self.children
+    print self.children.index(self.scanner)
 
 
   def add_scanner(self, target, dt):
